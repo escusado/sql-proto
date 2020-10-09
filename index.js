5f21289aa0ca8d5fd1840b10a577e1abb25ffd0c
toily <escusado@gmail.com>
2020-10-09 16:00:26 -0500

    adds routes

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ var connection = Mysql.createConnection({
 
 connection.connect();
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.get("/", (req, res) => {
   let contents = fs.readFileSync("index.html", "utf8");
 
@@ -25,10 +30,29 @@ app.get("/", (req, res) => {
 app.get("/list", (req, res) => {
   connection.query("SELECT * from proto", function (error, results, fields) {
     if (error) throw error;
-    console.log("The solution is: ", results);
+    // console.log("The solution is: ", results);
     res.send(results);
   });
 });
+
+app.post("/new", (req, res) => {
+  console.log("req", req.body);
+
+  connection.query(
+    `INSERT INTO proto (title, author, age) VALUES ("${req.body.title}", "${req.body.author}", ${req.body.age});`,
+    function (error, results, fields) {
+      if (error) throw error;
+      // console.log("The solution is: ", results);
+      console.log(">>>> done!");
+      res.send(results);
+    }
+  );
+});
+
+// app.post("/detail/id", (req, res) => {
+//   // ???????
+//   res.send(detail);
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
